62ab3bb5d4b3c10b0aa06f4d81db074584769505
austinwilson1224 <austin1224@gmail.com>
2020-09-21 15:56:57 -0700

    initial commit

---
+++ intro/script.js
@@ -0,0 +1,33 @@
+var x,y,z;
+
+x = "Milk";
+y = "Orange Juice";
+z = "Pineapple on Pizza is delicious";
+
+-1 // integer 
+0xFFFF // hexadecimal
+3.14 // float
+1 << 4 // bitshift
+
+// JSON
+{
+    name: "bob dylan"
+}
+
+// string
+"hello"
+true
+false 
+[1,2,3]
+'literal text, very cool!'
+
+var this_is_snake_case =  1;
+
+// console.log(this_is_snake_case);
+
+
+
+var name = 'Austin';
+var age = 27;
+console.log(name);
+console.log(age);
